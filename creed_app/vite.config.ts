dd64c3c1cc0fe30dbeca609a8f7f94251abee0f2
devanshsingh005 <devansh.singh20045@gmail.com>
2025-06-29 13:10:20 +0530

    netlify fix

--- creed_app/vite.config.ts
+++ creed_app/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // ensure it's pointing to the correct location
+  build: {
+    outDir: 'dist', // default is dist but safe to specify
+  }
 })
